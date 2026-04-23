9657860ec287e7e759194f7a7cb47d119f21a659
vmelonn <rayan.atif2@gmail.com>
2026-04-23 07:19:06 +0500

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/persona3-website/">
       <App />
     </BrowserRouter>
   </StrictMode>,
